f65fae402f43b108353303d1b77b37636c344f84
Jason Murphy <json.murphy@gmail.com>
2016-08-24 16:25:44 -0500

    Conskit and Project version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ck.mailer "0.1.0-SNAPSHOT"
+(defproject ck.mailer "1.0.0-rc1"
   :description "Mailer module for Conskit"
-  :url "https://website.com/example/ck.mailer"
+  :url "https://github.com/conskit/ck.mailer"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [conskit "0.2.0-SNAPSHOT"]
+                 [conskit "1.0.0-rc1"]
                  [clojurewerkz/mailer "1.3.0"]]
   :profiles {:dev {:source-paths ["dev"]
                    :resource-paths ["dev-resources"]
